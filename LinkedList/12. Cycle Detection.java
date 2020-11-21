94fd32c08c2ce5a3c1079f96138edf70778dc5ed
Neha <55926436+Neha-alt@users.noreply.github.com>
2020-11-21 16:16:49 +0530

    Create 12. Cycle Detection.java

---
+++ LinkedList/12. Cycle Detection.java
@@ -0,0 +1,14 @@
+//https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem
+
+static boolean hasCycle(SinglyLinkedListNode head) {
+        if(head==null || head.next==null) return false;
+        SinglyLinkedListNode fast = head;
+        SinglyLinkedListNode slow = head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow)
+            return true;
+        }
+        return false;
+    }
