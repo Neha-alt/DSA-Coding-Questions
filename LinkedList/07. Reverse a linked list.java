4d9d2e378a881b469ad1de92417a8999df2884b6
Neha <55926436+Neha-alt@users.noreply.github.com>
2021-03-15 01:10:31 +0530

    Update 07. Reverse a linked list.java

--- LinkedList/07. Reverse a linked list.java
+++ LinkedList/07. Reverse a linked list.java
@@ -1,15 +1,17 @@
-//https://www.hackerrank.com/challenges/reverse-a-linked-list/problem
+//https://leetcode.com/problems/reverse-linked-list/submissions/
 
-static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
-        SinglyLinkedListNode temp = head;
-        SinglyLinkedListNode prev= null;
-        SinglyLinkedListNode next = null;
-        while(temp!=null){
-            next = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = next;
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode frwd = null;
+        while(curr!=null){
+            frwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = frwd;
         }
         return prev;
-
     }
+}
